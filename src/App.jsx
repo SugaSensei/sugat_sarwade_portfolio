ed27c53d0633810891bea3066dd95476412824c9
SugaSensei <sugatraj.sarwade@gmail.com>
2025-04-18 00:10:42 +0530

    Add routing and HomePage component; update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Routes, Route } from 'react-router-dom'
+import HomePage from "./pages/HomePage"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className="text-[50px] text-center">Welcome to Sugatraj Sarwade's Portfolio</div>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+    </Routes>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: "/sugat_sarwade_portfolio"
+  base: "/sugat_sarwade_portfolio/"
 })
